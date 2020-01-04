df72d0280f74e2042546a0bf0f42d8e3eae40efa
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-04 20:19:02 +0900

    build binaries of multiple architecture

--- preprocess.sh
+++ preprocess.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 # define CLIENT_ID, CLIENT_SECRET as environment variable
-sed -i"" -e "s/{{CLIENT_ID}}/${CLIENT_ID}/" -e "s/{{CLIENT_SECRET}}/${CLIENT_SECRET}/" handlers_drive.go
+# specify .bak file to avoid difference mac and linux
+sed -i.bak -e "s/{{CLIENT_ID}}/${CLIENT_ID}/" -e "s/{{CLIENT_SECRET}}/${CLIENT_SECRET}/" handlers_drive.go
